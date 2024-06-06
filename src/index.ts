7ccfc81916368ea7cb2c1743aa9e862544a26cd2
Sam <31658121+samhoogantink@users.noreply.github.com>
2024-06-06 16:40:06 +0200

    Bumped multerize version

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,28 @@
 import { Hono } from 'hono';
-import { Multerize, HonoFileBodyEnv, R2StorageProvider, FileResult, FilesResultObject } from 'multerize';
+import { Multerize, MulterizeHonoBindings, R2StorageProvider, FileResult, FilesResultObject } from 'multerize';
+
+const upload = new Multerize({
+	storage: new R2StorageProvider<Env>({
+		// r2Client: env.R2_BUCKET, // You can import the bucket via this way
+		envBucketKey: 'R2_BUCKET', // Or you can use the env key, and we'll automatically import the bucket for you from the env
+		destination: async (c, file) => {
+			const randomFolders = ['folder1', 'folder2', 'folder3'];
+
+			// Select random folder
+			const randomFolder = randomFolders[Math.floor(Math.random() * randomFolders.length)];
+
+			return `${randomFolder}/`;
+		},
+		fileName: async (c, file) => {
+			const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+			return uniqueSuffix + '-' + file.originalName;
+		}
+	})
+});
 
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		const app = new Hono<{ Variables: HonoFileBodyEnv; }>();
-
-		const upload = new Multerize({
-			storage: new R2StorageProvider({
-				// r2Client: env.R2_BUCKET, // You can import the bucket via this way
-				envBucketKey: 'R2_BUCKET', // Or you can use the env key, and we'll automatically import the bucket for you from the env
-				destination: async (c, file) => {
-					const randomFolders = ['folder1', 'folder2', 'folder3'];
-
-					// Select random folder
-					const randomFolder = randomFolders[Math.floor(Math.random() * randomFolders.length)];
-
-					return `${randomFolder}/`;
-				},
-				fileName: async (c, file) => {
-					const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-    				return uniqueSuffix + '-' + file.originalName;
-				}
-			})
-		});
+		const app = new Hono<{ Variables: MulterizeHonoBindings; }>();
 
 		app.post('/profile', upload.single('avatar'), async (c) => {
 			const file = c.get('file');
